014815fbfa874b52e42348d36902a2e423783bf5
gameblabla <gameblabla@openmailbox.org>
2018-06-11 21:56:37 -0400

    Fix 16-bits colors on shadows

--- src/sdl2/renderbase.cpp
+++ src/sdl2/renderbase.cpp
@@ -36,17 +36,18 @@ void RenderBase::convert_palette(uint32_t adr, uint32_t r, uint32_t g, uint32_t 
 {
     adr >>= 1;
 
-    r = (r * (255 / 31));
-    g = (g * (255 / 31));
-    b = (b * (255 / 31));
-    
-    rgb[adr] = (r << 8 | g << 3 | b >> 3);
+    r = (r) * (255 / 31);
+    g = (g) * (255 / 31);
+    b = (b) * (255 / 31);
+
+	rgb[adr] = ((r & 0b11111000) << 8) | ((g & 0b11111100) << 3) | (b >> 3);
       
     // Create shadow / highlight colours at end of RGB array
     // The resultant values are the same as MAME
-    r = (r * 0.791);
-    g = (g * 0.791);
-    b = (b * 0.791);
+    // 79105
+    r = (r * 202) / 256;
+    g = (g * 202) / 256;
+    b = (b * 202) / 256;
     
-    rgb[adr + S16_PALETTE_ENTRIES] = rgb[adr + (S16_PALETTE_ENTRIES * 2)] = (r << 8| g << 3| b >> 3);
+    rgb[adr + S16_PALETTE_ENTRIES] = rgb[adr + (S16_PALETTE_ENTRIES * 2)] = ((r & 0b11111000) << 8) | ((g & 0b11111100) << 3) | (b >> 3);
 }
